9aa60a42aecd0b84af7061bb815cb3487ae96e29
Philipp Winkler <philipp.winkler@pludoni.de>
2018-12-14 09:43:52 +0100

    Model-Exchanger + HTTP-Post hinzugefügt

---
+++ lib/model_exchanger/version.rb
@@ -0,0 +1,3 @@
+module ModelExchanger
+  VERSION = "0.1.0"
+end
---
+++ spec/model_exchanger_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ModelExchanger do
+  it "has a version number" do
+    expect(ModelExchanger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
