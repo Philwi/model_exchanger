c54cb75c6b108473b9f22dea5c7fb1342058ea9b
Philipp Winkler <philipp.winkler@pludoni.de>
2018-12-17 09:50:16 +0100

    net/http

--- lib/model_exchanger/send.rb
+++ lib/model_exchanger/send.rb
@@ -1,12 +1,12 @@
+require 'net/http'
 
 module ModelExchanger
-  require 'net/http'
   class Send
     def initialize(get_associations, url, service)
       uri = URI(url)
-      http = Class::NET::HTTP.new(uri.host, uri.port)
-      req = Class::NET::HTTP::POST.new(uri)
-      res = Class::NET::HTTP.post_form(uri, 'hash' => get_associations, 'service' => service)
+      http = Class::Net::HTTP.new(uri.host, uri.port)
+      req = Class::Net::HTTP::POST.new(uri)
+      res = Class::Net::HTTP.post_form(uri, 'hash' => get_associations, 'service' => service)
       puts res.body
     end
   end
