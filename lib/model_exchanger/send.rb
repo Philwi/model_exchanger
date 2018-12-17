d6c4503adfbf596bf30e54cc676a7588403605fa
Philipp Winkler <philipp.winkler@pludoni.de>
2018-12-17 10:46:06 +0100

    net/http

--- lib/model_exchanger/send.rb
+++ lib/model_exchanger/send.rb
@@ -6,7 +6,8 @@ module ModelExchanger
       uri = URI(url)
       http = Net::HTTP.new(uri.host, uri.port)
       req = Net::HTTP::Post.new(url)
-      res = Net::HTTP.post_form(uri, 'hash' => get_associations, 'service' => service)
+      #res = Net::HTTP.post_form(uri, 'hash' => get_associations, 'service' => service)
+      res = Net::HTTP.get("#{url}?hash=get_associations&service=service")
       puts res.body
     end
   end
